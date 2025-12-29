1878c553b529b317b5a3fefd6def671968452d4a
Richard Tiphaine <tiphaine@mac.home>
2025-12-29 11:42:42 +0100

    css

--- app.py
+++ app.py
@@ -2,44 +2,59 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/periode1')
+
+@app.route("/periode1")
 def periode1():
-    return render_template('periode1.html', 
-                         title="L'hégémonie imposée (1945-1960)")
+    return render_template(
+        "periode1.html", title="L'hégémonie imposée (1945-1960)"
+    )
 
-@app.route('/periode2') 
+
+@app.route("/periode2")
 def periode2():
-    return render_template('periode2.html',
-                         title="L'hégémonie autoritaire (1960-1987)")
+    return render_template(
+        "periode2.html", title="L'hégémonie autoritaire (1960-1987)"
+    )
 
-@app.route('/periode3')
+
+@app.route("/periode3")
 def periode3():
-    return render_template('periode3.html',
-                         title="L'hégémonie démocratique (1987-2000)")
+    return render_template(
+        "periode3.html", title="L'hégémonie démocratique (1987-2000)"
+    )
 
-@app.route('/periode4')
+
+@app.route("/periode4")
 def periode4():
-    return render_template('periode4.html',
-                         title="L'hégémonie négociée (2000-2020)")
+    return render_template(
+        "periode4.html", title="L'hégémonie négociée (2000-2020)"
+    )
 
-@app.route('/ressources')
+
+@app.route("/ressources")
 def ressources():
-    return render_template('ressources.html',
-                         title="Sources et ressources")
-@app.route('/jeju')
+    return render_template("ressources.html", title="Sources et ressources")
+
+
+@app.route("/jeju")
 def jeju():
-    return render_template('jeju.html', title='Le massacre de Jeju')
+    return render_template("jeju.html", title="Le massacre de Jeju")
 
-@app.route('/conclusion')
+
+@app.route("/conclusion")
 def conclusion():
-    return render_template('conclusion.html', title='Conclusion')
-@app.route('/interview')
-def interview():
-    return render_template('interview.html', title='Interview avec Eom Hyunsoo')
+    return render_template("conclusion.html", title="Conclusion")
 
-if __name__ == '__main__':
+
+@app.route("/interview")
+def interview():
+    return render_template("interview.html", title="Interview avec Eom Hyunsoo")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
